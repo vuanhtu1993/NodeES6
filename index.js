ec616318b06e69711f32c32e2a1f525fc5897ca8
vuanhtu1993mta@gmail.com <tuntun159753>
2018-02-12 14:13:01 +0700

    1. Learning debugger in nodejs

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Calculator aria and circumference of rectangle
 const rect = require('./shape/rectangle');
 console.log('Aria',rect.aria(10, 20));
 console.log('Circumference',rect.circumference(10, 20));
 console.log('Date time',rect.currentTime);
+
+// Tinh trung binh
+let arithmeticMean = (arr) => {
+    debugger;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + Number(arr[i]);
+        debugger;
+    }
+    return sum/arr.length;
+};
+console.log(arithmeticMean([1, 2, 3, 4, 5, 6]));
