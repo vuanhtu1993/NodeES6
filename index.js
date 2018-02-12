51a6cab4205e275ce3e7d84dc77d253fe3fdd3a3
vuanhtu1993mta@gmail.com <tuntun159753>
2018-02-12 13:45:23 +0700

    1. exports.function public this function 2. module.exports Consider a file as one module and public thís module

---
+++ index.js
@@ -0,0 +1,4 @@
+const rect = require('./shape/rectangle');
+console.log('Aria',rect.aria(10, 20));
+console.log('Circumference',rect.circumference(10, 20));
+console.log('Date time',rect.currentTime);
---
+++ shape/rectangle.js
@@ -0,0 +1,10 @@
+// // aria function
+// exports.aria = (width, height) => width * height;
+// //circumference
+// exports.circumference = (width, height) => 2 * (width + height);
+
+module.exports = {
+    aria: (width, height) => width * height,
+    circumference: (width, height) => 2 * (width + height),
+    currentTime: Date(),
+};
